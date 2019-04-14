7a3ffcf43a80fd5bf35dc3c62b4727e97c0ce0e4
alexnsaba <alexnewzniyo@gmail.com>
2019-04-14 11:10:20 -0700

    interface dependencies

---
+++ includes/sidebar.php
@@ -0,0 +1,101 @@
+<!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">         
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MANAGER'S DASH BOARD</li>
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-users"></i> <span>MANAGE USERS</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="#"><i class="fa fa-user"></i> Create Account</a></li>
+            <li><a href="#"><i class="fa fa-edit"></i> Edit Account</a></li>
+			<li><a href="#"><i class="fa fa-trash-o"></i> Delete Account</a></li>
+			<li><a href="#"><i class="fa fa-newspaper-o"></i> View All Accounts</a></li>
+          </ul>
+		  
+        </li>
+		
+		  <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-bank"></i> <span>PURCHASES/PRODUCTS</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="#"><i class="fa fa-circle-o"></i> Stoke-In</a></li>
+            <li><a href="#"><i class="fa fa-trash-o"></i> Stoke-Out</a></li>
+			<li><a href="#"><i class="fa fa-newspaper-o"></i>Products Available</a></li>
+			<li><a href="#"><i class="fa fa-edit"></i>Edit Products</a></li>		
+		   </ul>
+		  
+        </li>
+		
+		 <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-cc-mastercard"></i> <span>SALES</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="#"><i class="fa fa-plus"></i>New Transaction</a></li>
+			<li><a href="admin.php"><i class="fa fa-newspaper-o"></i> view Transactions</a></li>
+            <li><a href="#"><i class="fa fa-edit"></i> Edit Transaction</a></li>
+			<li><a href="#"><i class="fa fa-trash-o"></i>Delete Transaction</a></li>			
+          </ul>
+		  
+        </li>
+		
+		 <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-usd"></i> <span>EXPENSES</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="#"><i class="fa fa-plus"></i>New Expense</a></li>
+			<li><a href="#"><i class="fa fa-newspaper-o"></i> view Expenses</a></li>
+            <li><a href="#"><i class="fa fa-edit"></i> Edit Expenses</a></li>
+			<li><a href="#"><i class="fa fa-trash-o"></i>Delete Expenses</a></li>			
+          </ul>
+		  
+        </li>
+		 <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-balance-scale"></i> <span>ACCOUNTING</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>       
+		</li>
+		
+		
+		 <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-line-chart"></i> <span>REPORTS</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="#"><i class="fa fa-pie-chart"></i>Daily Report</a></li>
+			<li><a href="#"><i class="fa fa-bar-chart-o (alias)"></i> Monthly Report</a></li>
+            <li><a href="#"><i class="fa fa-area-chart"></i> Annual Report</a></li>
+			<li><a href="#"><i class="fa fa-circle-o"></i>Emmergency Report</a></li>			
+          </ul>
+		  
+        </li>
+		
+		</ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
